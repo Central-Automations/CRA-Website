680a90cf40700d38b4a007c85d9ea6b202509a07
Central-Automations <rashid@centralautomations.com>
2025-08-23 15:27:08 +0400

    Update contact-form.tsx

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -27,12 +27,12 @@ export default function ContactForm() {
         </p>
 
         {/* Calendly inline widget begin */}
-        <div
+{/*         <div
           className="calendly-inline-widget"
           data-url="https://calendly.com/rashid-centralautomations/discovery?hide_event_type_details=1"
           style={{ minWidth: "320px", height: "700px" }}
           data-testid="calendly-widget"
-        />
+        /> */}
         {/* Calendly inline widget end */}
       </div>
     </section>
