0a610c47272525c36565c3b69447013b8a54fd71
Central-Automations <rashid@centralautomations.com>
2025-09-08 19:01:18 +0400

    Update contact-form.tsx

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -26,22 +26,13 @@ export default function ContactForm() {
           our team to explore how we can help grow your recruitment agency.
         </p>
 
-
-        <div style="width:100%;height:100%;overflow:scroll" id="my-cal-inline-discovery"></div>
-        <script type="text/javascript">
-          (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
-        Cal("init", "discovery", {origin:"https://app.cal.com"});
-        
-          Cal.ns.discovery("inline", {
-            elementOrSelector:"#my-cal-inline-discovery",
-            config: {"layout":"month_view","theme":"light"},
-            calLink: "central-automations/discovery",
-          });
-        
-          Cal.ns.discovery("ui", {"theme":"light","cssVarsPerTheme":{"light":{"cal-brand":"#ff7200"},"dark":{"cal-brand":"#ff7200"}},"hideEventTypeDetails":true,"layout":"month_view"});
-          </script>
-
-        
+        {/* Calendly inline widget begin */}
+        <div
+          className="calendly-inline-widget"
+          data-url="https://calendly.com/rashid-centralautomations/discovery?hide_event_type_details=1"
+          style={{ minWidth: "320px", height: "700px" }}
+          data-testid="calendly-widget"></div>
+        {/* Calendly inline widget end */}
       </div>
     </section>
   );
